00896548efb441b996835b250bf865cb175dd0e1
kleinfreund <phrudloff@gmail.com>
2017-08-25 15:15:47 +0200

    Initial commit (move development repository to GitHub)

--- README.md
+++ README.md
@@ -1,2 +1,135 @@
-# slidehub
-Generic code for slidehub pages
+CODE-WEBIS-WEB-SLIDEHUB
+=======================
+
+SlideHub is a web-based overview of paged documents (i.e. slide decks of a presentation/talk/lecture/…). Its goal is to provide quick access to a database of documents.
+
+Each document is converted to a set of graphics—one for each page. These graphics are arranged in a grid-like structure where each row represents a document. The cells of a row are the pages of that document.
+
+
+
+Features
+--------
+
+- Documents are listed row-wise with pages being displayed in a horizontal slider
+- Horizontal scrolling of slide decks via the mouse wheel
+- Vertical scrolling via dragging
+- Lazy-loading of slide images
+
+
+
+Navigation
+----------
+
+The application needs to be usable on mobile and desktop devices utilizing the present primary input techniques.
+
+- mouse input
+- touchpad input
+- keyboard input (lower priority)
+
+The challenge is implementing a navigation pattern for a layout that is interactive in two dimensions. While a keyboard offers a variety of possibilities for doing so, a mouse is more limited. It’s safe to assume a mouse with a primary and secondary mouse button and a scrolling wheel with the ability to trigger a click as well. That said, browsers already leverage these buttons for navigation of a web page.
+
+The scrolling wheel moves the viewport across the vertical axis while the primary mouse button is used for all sorts of interaction on the page (e.g. opening links, clicking buttons, etc.). The secondary mouse button is mostly used for opening a context menu in order to invoke browser-specific features.
+
+### Current Concept
+
+#### Mouse
+
+Navigating through pages of documents on the horizontal axis is done by using the scrolling wheel with a configurable modifier. It’s advised to not use <kbd>Ctrl</kbd> since most user agents change the zoom level with <kbd>Ctrl</kbd><kbd>ScrollingWheel</kbd>.
+
+#### Keyboard
+
+The arrow keys are used to navigate across documents and pages. Vertical navigation across documents is done with <kbd>↑</kbd> and <kbd>↓</kbd>. Horizontal navigation across pages is done with <kbd>←</kbd> and <kbd>→</kbd>.
+
+Additionally, <kbd title="tab key">⇥</kbd> and <kbd>Shift</kbd><kbd title="tab key">⇥</kbd> navigate across documents as well.
+
+
+
+TODOs
+-----
+
+- Title bar that is always displayed with some basic information
+- Organization of slides by, e.g., topic, year, etc.
+- Prepended "slide" that displays meta information such as year, venue
+- Linking to the actual PDF and its pages from each slide
+- Selection/highlighting of slides (already supported by Sly library)
+- Responsiveness (esp. touch devices)
+- Dragscrolling with ballistics
+- Dynamic determination of the number of slides per slide deck
+- Dynamic loading of slide decks based on our publications infrastructure
+- Study the quality-size trade-off of slide PNGs
+- Store often-used properties globally (e.g. page width)
+- Implement horizontal scrolling for document containers
+- <s>Re-calculate page item sizes to avoid sub-pixel values</s>
+
+
+
+Known Bugs
+----------
+
+- Scalability: Many slide decks cause the initialization script to run very long (Dynamically loading documents [e.g. like infinite scrolling] is doable with `IntersectionObserver`)
+- ImageMagick occasionally creates transparent PNGs (For documents with a specific background color, this might have a drastic impact on perceivability)
+- documents `unit-en-radial-basis-functions.pdf`, `unit-de-conceptual-design3.pdf`, `unit-de-relational-design0.pdf` have no PNGs (e.g. all entries with only one page)
+- Holding the modifier key currently shows an `ew-resize` cursor on containers. That signifies a click-and-drag interaction instead of the intended scrolling interaction. However <kbd>Alt</kbd><kbd>Click-dragging</kbd> on Ubuntu is an OS-level feature that allows dragging a window. It cannot be disabled via JavaScript’s `event.preventDefault()`. Therefor, the `ew-resize` cursor is not appropriate.
+- `overflow: hidden;` on a document container hides the scrollbars as intended but disables scrolling which is not intended on mobile platforms. Possible solutions: Show scrollbars on desktop platforms *or* reimplement horizontal scrolling on mobile platforms (requires UA-sniffing)
+- <s>Adding new documents after the initial scripts are already executed results in non-interactive documents since no event listener have been attached etc. (The initialization logic needs to re-evaluatable) → Adding/removing documents needs to trigger a reevaluation of all kinds of event listeners etc.</s>
+- <s>Lazy-loading doesn’t work reliably when `img` elements are nested in `div`’s (Needs further investigation)</s> (Fixed via af5b3017)
+- <s>The horizontal sliders do not align correctly with the slide matrix when scrolling to the end</s> (Fixed by the new slider logic)
+- <s>In Chrome, dragscrolling sometimes selects content despite the CSS rules preventing that, causing hickups</s> (Fixed via c238ea31)
+
+
+
+Ideas
+-----
+
+- When multiple interactions are possible, we need to keep track of which component has the *interaction focus*. For example, the document explorer should have the interaction focus by default (enabling arrow key navigation). If a filter component has focus, the keyboard navigation need not trigger (in order to allow usual keys to work in input fields)
+
+
+
+Generate graphics from PDF files
+--------------------------------
+
+[ImageMagick](https://www.imagemagick.org) is capable of a wide variety of image manipulation tasks. The following examples use the typical way of invoking it via the command line.
+
+First of all, the package needs to be installed.
+
+```
+sudo apt-get install imagemagick
+```
+
+### Examples
+
+**Converting a single file**:
+
+```
+convert -density 72 -quality 90 slides.pdf slides.pdf.png
+```
+
+The input file is a PDF with a certain amount of pages. Therefor, this command produces as many graphics as there are pages in the document. Without specifyingh further arguments, an index suffic will be added to the file name (e.g. `slides-0.png`, etc.).
+
+**Converting multiple files**:
+
+The `convert` command can be called from inside a loop to process multiple files.
+
+```
+for f in *.pdf; do convert -density 72 -quality 90 $f $f.png; done
+```
+
+Alternatively, the `mogrify` command covers the looping part as well.
+
+```
+mogrify -format png -density 72 -quality 90 *.pdf
+```
+
+**Counting the number of graphics per document**:
+
+```
+for f in *.pdf; do echo $f; ls -l $f*.png | wc -l; done
+```
+
+**Resizing images to a specific width**
+
+The following command shrinks all files with the `png` extension to a width of 600 pixels while maintaining the aspect ratio. Smaller files are not resized.
+
+```
+mogrify -resize '600x>' *.png
+```
---
+++ js/script.js
@@ -0,0 +1,515 @@
+'use strict'
+
+/*
+* Configuration
+*/
+const config = {
+    itemWidth: 300,
+    minimalDocumentHeight: true,
+    class: {
+        main: '.main-content',
+        container: '.doc-view',
+        document: '.doc',
+        item: '.doc__page'
+    },
+    modifierKey: 'altKey'
+}
+
+let imgSrcRoot
+let containerObserver
+let visibleItems
+
+// Maps key codes to key names
+const modifierKeyNames = Object.freeze({
+    16: 'shiftKey',
+    17: 'ctrlKey',
+    18: 'altKey'
+})
+
+/*
+* Maps key codes to key names.
+* It’s used within keyboard-related event handlers in order to work with the
+* keys’ names instead of key codes.
+*
+* Removing an entry here disables its application-related interactions
+*/
+const controlKeyName = Object.freeze({
+    // 9: 'tabKey',
+    35: 'endKey',
+    36: 'homeKey',
+    37: 'arrowLeft',
+    38: 'arrowUp',
+    39: 'arrowRight',
+    40: 'arrowDown'
+})
+
+/*
+* Maps control keys to a trigger function that is executed when the key is
+* pressed.
+*/
+const controlKey = Object.freeze({
+    homeKey: {
+        trigger: function(event) {
+            goToItem(0)
+        }
+    },
+    endKey: {
+        trigger: function(event) {
+            goToItem(getLastPage())
+        }
+    },
+    arrowLeft: {
+        direction: -1,
+        trigger: function(event) {
+            const count = event.ctrlKey ? 3 : 1
+            moveByItems(this.direction, count)
+        }
+    },
+    arrowRight: {
+        direction: 1,
+        trigger: function(event) {
+            const count = event.ctrlKey ? 3 : 1
+            moveByItems(this.direction, count)
+        }
+    },
+    arrowUp: {
+        trigger: function(event) {
+            goToPreviousContainer()
+        }
+    },
+    arrowDown: {
+        trigger: function(event) {
+            goToNextContainer()
+        }
+    },
+    tabKey: {
+        trigger: function(event) {
+            if (event.shiftKey) {
+                goToPreviousContainer()
+            } else {
+                goToNextContainer()
+            }
+        }
+    }
+})
+
+function initialize(srcRoot) {
+    imgSrcRoot = srcRoot
+    initializeLazyLoader()
+
+    document.addEventListener('DOMContentLoaded', function() {
+        enableModalButtons()
+        enableKeyboardNavigation()
+        enableMousePageNavigation()
+        activateDocumentWithFocus()
+        focusDocumentWithMouseMove()
+
+        loadDocumentAsync()
+            .then(onFirstDocumentLoaded, onDocumentReject)
+            .catch(message => { console.error(message) })
+    })
+}
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// LOADING DOCUMENTS
+
+function loadDocumentAsync() {
+    return new Promise((resolve, reject) => {
+        const mainContent = document.querySelector(config.class.main)
+        const docData = documentsData[0]
+
+        if (docData === undefined) {
+            reject('No more documents to load.')
+        }
+
+        loadDoc(mainContent, docData)
+        const container = mainContent.lastElementChild
+        containerObserver.observe(container)
+        setDocumentWidth(container.querySelector(config.class.document))
+        resolve()
+    })
+}
+
+function loadDoc(mainContent, docData) {
+    const containerTemplate = document.createElement('template')
+    containerTemplate.innerHTML = createDocumentMarkup(docData[0], docData[1])
+    mainContent.appendChild(containerTemplate.content)
+}
+
+/*
+* Creates the full markup of one document
+*/
+function createDocumentMarkup(docName, itemCount) {
+    let items = ''
+    for (var i = 0; i < itemCount; i++) {
+        const source = `${imgSrcRoot}/data/${docName}-${i}.png`
+        items += `<div class="${config.class.item.slice(1)}">
+            <img data-src="${source}" alt="page ${i + 1}">
+        </div>`
+    }
+
+    return `<div class="${config.class.container.slice(1)}" id="${docName}">
+        <div class="${config.class.document.slice(1)}">
+            <div class="${config.class.item.slice(1)} doc-info">
+                <h3>${docName}</h3>
+                <span>${itemCount} pages</span>
+            </div>
+            ${items}
+        </div>
+    </div>`
+}
+
+function setDocumentWidth(doc) {
+    const documentOuterWidth =
+        getFloatPropertyValue(doc, 'margin-left') +
+        getFloatPropertyValue(doc, 'border-left-width') +
+        getFloatPropertyValue(doc, 'padding-left') +
+        getComputedOuterChildrenWidth(doc) +
+        getFloatPropertyValue(doc, 'padding-right') +
+        getFloatPropertyValue(doc, 'border-right-width') +
+        getFloatPropertyValue(doc, 'margin-right')
+    doc.style.setProperty('width', documentOuterWidth + 'px')
+}
+
+function onFirstDocumentLoaded() {
+    const firstContainer = document.querySelector(config.class.container)
+    activateContainer(firstContainer)
+
+    evaluateItemWidth()
+    setFullyVisibleItems()
+    onDocumentLoaded()
+}
+
+function onDocumentLoaded() {
+    documentsData.shift() // Delete first element
+    console.info('Document loaded. Remaining:', documentsData.length)
+    loadDocumentAsync()
+        .then(onDocumentLoaded, onDocumentReject)
+        .catch(message => { console.log(message) })
+}
+
+function onDocumentReject(message) {
+    console.info(message)
+}
+
+function evaluateItemWidth() {
+    const itemSample = document.querySelector(config.class.item)
+    const itemOuterWidth = getOuterWidth(itemSample)
+    // const itemOuterWidth = Math.ceil(getOuterWidth(itemSample))
+
+    if (itemOuterWidth !== config.itemWidth) {
+        console.info(
+            'Pre-configured page width does not match actual page width.',
+            'Updating configuration.'
+        )
+        config.itemWidth = itemOuterWidth
+    }
+}
+
+function setFullyVisibleItems() {
+    visibleItems = getFullyVisibleItems()
+}
+
+function getFullyVisibleItems() {
+    const itemSample = document.querySelector(config.class.item)
+    const itemOuterWidth = getOuterWidth(itemSample)
+
+    const containerSample = document.querySelector(config.class.container)
+    const containerWidth = getFloatPropertyValue(containerSample, 'width')
+    return Math.floor(containerWidth / itemOuterWidth)
+}
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// NAVIGATION
+
+function enableKeyboardNavigation() {
+    document.addEventListener('keydown', function(event) {
+        // Get key name from key code
+        const keyName = controlKeyName[event.keyCode]
+
+        // If the pressed key is no control key …
+        if (keyName === undefined) {
+            return
+        }
+
+        event.preventDefault()
+
+        controlKey[keyName].trigger(event)
+    })
+}
+
+function activateDocumentWithFocus() {
+    document.addEventListener('focusin', function(event) {
+        const container = event.target.closest(config.class.container)
+        if (container !== null) {
+            activateContainer(container)
+        }
+    })
+}
+
+function focusDocumentWithMouseMove() {
+    document.addEventListener('mousemove', function(event) {
+        const container = event.target.closest(config.class.container)
+        if (container !== null) {
+            activateContainer(container)
+        }
+    })
+}
+
+function enableModifier() {
+    const modifier = config.modifierKey.replace('Key', '')
+    document.documentElement.setAttribute('data-modifier', modifier)
+    const modifierKBDElements = document.querySelectorAll('.shortcut__modifier')
+    modifierKBDElements.forEach(el => el.innerText = modifier)
+    // kbdEl.innerText = modifier
+
+    document.addEventListener('keydown', function(event) {
+        const modifier = modifierKeyNames[event.keyCode]
+        if (modifier === config.modifierKey) {
+            document.documentElement.classList.add('modifier-pressed')
+        }
+    })
+
+    document.addEventListener('keyup', function(event) {
+        const modifier = modifierKeyNames[event.keyCode]
+        if (modifier === config.modifierKey) {
+            document.documentElement.classList.remove('modifier-pressed')
+        }
+    })
+
+    window.addEventListener('blur', function(event) {
+        document.documentElement.classList.remove('modifier-pressed')
+    })
+}
+
+function enableMousePageNavigation() {
+    enableModifier()
+
+    document.addEventListener('wheel', function(event) {
+        // No special scrolling without modifier
+        if (event[config.modifierKey] === false) {
+            return
+        }
+
+        // No special scrolling when not scrolling vertically
+        if (event.deltaY === 0) {
+            return
+        }
+
+        const container = event.target.closest(config.class.container)
+        if (container === null) {
+            return
+        }
+
+        // Prevent vertical scrolling
+        event.preventDefault()
+
+        // Prevent unnecessary actions when there is nothing to scroll
+        const numItems = container.querySelector(config.class.document).childElementCount
+        if (numItems <= visibleItems) {
+            return
+        }
+
+        moveByItems(Math.sign(event.deltaY))
+    })
+}
+
+function moveByItems(direction, count = 1) {
+    const container = getActiveContainer()
+    if (container === null) {
+        return
+    }
+
+    const itemsBeforeScrollPos = container.scrollLeft / config.itemWidth
+    let nextPage = 0
+    if (itemsBeforeScrollPos % 1 !== 0) {
+        const roundOp = direction > 0 ? Math.ceil : Math.floor
+        nextPage = roundOp(itemsBeforeScrollPos) + direction * count
+    } else {
+        nextPage = itemsBeforeScrollPos + direction * count
+    }
+
+    goToItem(nextPage)
+}
+
+function goToItem(item) {
+    const container = getActiveContainer()
+    if (container !== null) {
+        container.scrollLeft = item * config.itemWidth
+    }
+}
+
+function goToPreviousContainer() {
+    const target = getActiveContainer().previousElementSibling
+    if (target !== null) {
+        activateContainer(target)
+    }
+}
+
+function goToNextContainer() {
+    const target = getActiveContainer().nextElementSibling
+    if (target !== null) {
+        activateContainer(target)
+    }
+}
+
+function getLastPage() {
+    const container = getActiveContainer()
+    return container.querySelectorAll(config.class.item).length - 1
+}
+
+function getActiveContainer() {
+    // First check for an element holding the `active` class
+    const activeElement = document.querySelector('.active')
+    if (activeElement !== null) {
+        return activeElement
+    }
+
+    const containerClassName = config.class.container.slice(1)
+    if (document.activeElement.classList.contains(containerClassName)) {
+        return document.activeElement
+    }
+
+    return null
+}
+
+function activateContainer(container) {
+    document.querySelectorAll('.active').forEach(el => el.classList.remove('active'))
+    container.classList.add('active')
+    // container.focus()
+    // container.scrollIntoView(false)
+}
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// MODAL WINDOW
+function enableModalButtons() {
+    const shortcutsButton = document.querySelector('.open-shortcuts-modal')
+    const shortcutsModal = document.querySelector('.modal--shortcuts')
+
+    shortcutsButton.addEventListener('click', function() {
+        shortcutsModal.classList.toggle('closed')
+    })
+
+    document.addEventListener('keydown', function(event) {
+        if (event.keyCode === 27) {
+            shortcutsModal.classList.add('closed')
+        }
+    })
+
+    shortcutsModal.addEventListener('click', function(event) {
+        if (event.target === shortcutsModal) {
+            shortcutsModal.classList.add('closed')
+        }
+    })
+}
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// LAZY LOADING PAGES
+
+/*
+* Observes document containers in order to load their page images only when
+* they’re visible.
+*/
+function initializeLazyLoader() {
+    const options = {
+        rootMargin: `500px 0px`
+    }
+
+    containerObserver = new IntersectionObserver(containerHandler, options)
+}
+
+function containerHandler(entries, observer) {
+    for (const entry of entries) {
+        if (entry.isIntersecting) {
+            const containerImages = entry.target.querySelectorAll('img[data-src]')
+            // For each image …
+            containerImages.forEach(img => {
+                // … swap out the `data-src` attribute with the `src` attribute.
+                // This will start loading the images.
+                if (img.hasAttribute('data-src')) {
+                    img.setAttribute('src', img.getAttribute('data-src'))
+                    img.removeAttribute('data-src')
+                }
+            })
+
+            containerImages[0].addEventListener('load', function() {
+                handleFirstItemImageLoaded(entry.target)
+            })
+
+            // Unobserve the current target because no further action is required
+            observer.unobserve(entry.target)
+        }
+    }
+}
+
+function handleFirstItemImageLoaded(container) {
+    if (config.minimalDocumentHeight) {
+        setPageAspectRatio(container)
+    }
+}
+
+function setPageAspectRatio(container) {
+    const imgSample = container.querySelector(`${config.class.item} > img`)
+    const aspectRatio = imgSample.naturalWidth / imgSample.naturalHeight
+    container.style.setProperty('--page-aspect-ratio', aspectRatio)
+}
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// MISC
+
+function getFloatPropertyValue(element, property) {
+    const value = getComputedStyle(element).getPropertyValue(property)
+
+    if (value === '') {
+        return 0
+    }
+
+    return parseFloat(value)
+}
+
+/*
+* Computes the total outer width of an element by accumulating its children’s
+* horizontal dimension property values (i.e. margin-left, width, margin-right)
+*/
+function getComputedOuterChildrenWidth(element) {
+    let outerWidth = 0
+
+    Array.from(element.children).forEach(child => {
+        outerWidth += getOuterWidth(child)
+    })
+
+    return outerWidth
+}
+
+/*
+* Computes the total outer width of an element.
+* The outer width is defined as the width plus any horizontal margins.
+* This is assumes the the `box-sizing` box model.
+*/
+function getOuterWidth(element) {
+    const width = getFloatPropertyValue(element, 'width')
+    const marginLeft = getFloatPropertyValue(element, 'margin-left')
+    const marginRight = getFloatPropertyValue(element, 'margin-right')
+
+    return marginLeft + width + marginRight
+}
