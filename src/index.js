a99b55d2764b0955c18220478342a96cafcf4419
kleinfreund <phrudloff@gmail.com>
2017-09-13 21:33:08 +0200

    Start splitting CSS into modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import './css/generic/base.css';
+import './css/generic/variables.css';
+import './css/layout/form-elements.css';
 import './css/style.css';
+
 import './js/intersection-observer.js';
 import initialize from './js/script.js';
 
