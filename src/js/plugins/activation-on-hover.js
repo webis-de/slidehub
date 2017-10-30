23c9620235e6bd9d22f53a91914f09f6860e4eb4
kleinfreund <phrudloff@gmail.com>
2017-10-30 10:26:47 +0100

    Don’t move active state with touch-based pointers

--- src/js/plugins/activation-on-hover.js
+++ src/js/plugins/activation-on-hover.js
@@ -4,19 +4,37 @@ import { setActiveView, setActiveItem } from '../core/view-navigation';
 
 export { ActivationOnHover };
 
+let touched = false;
+
 const ActivationOnHover = {
   enabled: true,
   name: 'activation-on-hover',
   description: 'Activate pages on hover',
   enable() {
+    document.addEventListener('touchstart', setTouched, listener.passive);
     document.addEventListener('mousemove', handleActivationOnHover, listener.passive);
+    document.addEventListener('mouseup', resetTouched, listener.passive);
   },
   disable() {
+    document.removeEventListener('touchstart', setTouched, listener.passive);
     document.removeEventListener('mousemove', handleActivationOnHover, listener.passive);
+    document.removeEventListener('mouseup', resetTouched, listener.passive);
   }
 };
 
+function setTouched() {
+  touched = true;
+}
+
+function resetTouched() {
+  touched = false;
+}
+
 function handleActivationOnHover(event) {
+  if (touched) {
+    return;
+  }
+
   const view = event.target.closest(config.selector.view);
   const item = event.target.closest(config.selector.item);
 
