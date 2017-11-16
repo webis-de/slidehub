8f90dd25648501d5a9e272034c245b14d84f65ed
kleinfreund <phrudloff@gmail.com>
2017-11-16 10:35:44 +0100

    Adopt ITCSS structure for organizing CSS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-import './css/generic/variables.css';
-import './css/generic/base.css';
-
-import './css/objects/form-elements.css';
-import './css/objects/modal.css';
-import './css/objects/skip-link.css';
-
-import './css/layout/main.css';
-import './css/layout/toolbox.css';
-import './css/layout/shortcut-table.css';
+import './css/slidehub.css';
 
 import './js/app.js';
