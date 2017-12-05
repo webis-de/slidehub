0f755a163d93e3783ba252771e75d8cf548a8f1d
kleinfreund <phrudloff@gmail.com>
2017-12-05 15:18:28 +0100

    Add type hints to util/clamp

--- src/js/util/clamp.js
+++ src/js/util/clamp.js
@@ -1,3 +1,14 @@
+/**
+ * Clamps the given value between the min and max boundaries
+ * Returns value if min <= value <= max.
+ * Returns min if value < min.
+ * Returns max if value > max.
+ *
+ * @param {number} value
+ * @param {number} min
+ * @param {number} max
+ * @return {number}
+ */
 export function clamp(value, min, max) {
   return Math.max(min, Math.min(value, max));
 }
