5fbe5af051e726981471b51bec94fda1809fc8c3
kleinfreund <phrudloff@gmail.com>
2018-05-19 12:26:29 +0200

    Make PageWidgetPlugin.disable more robust

--- src/js/plugins/PageWidgetPlugin.js
+++ src/js/plugins/PageWidgetPlugin.js
@@ -10,43 +10,48 @@ class PageWidgetPlugin extends SlidehubPlugin {
 
     this.boundExposeItemCount = this.exposeItemCount.bind(this);
     this.boundExposeCurrentItem = this.exposeCurrentItem.bind(this);
+    this.widgetHookNode = document.querySelector('[data-slidehub-page-widget]');
     this.currentPageNode = null;
     this.totalPagesNode = null;
   }
 
   enable() {
-    this.insertMarkup();
+    this.insertWidgetMarkup();
     this.slidehub.node.addEventListener('SlidehubHoverDocument', this.boundExposeItemCount, listener.passive);
     this.slidehub.node.addEventListener('SlidehubHoverItem', this.boundExposeCurrentItem, listener.passive);
     super.enable();
   }
 
   disable() {
-    this.removeMarkup();
+    this.removeWidgetMarkup();
     this.slidehub.node.removeEventListener('SlidehubHoverDocument', this.boundExposeItemCount);
     this.slidehub.node.removeEventListener('SlidehubHoverItem', this.boundExposeCurrentItem);
     super.disable();
   }
 
-  insertMarkup() {
-    const widgetMarkup = `<div class="sh-page-widget">
-      <span class="sh-page-widget__current-page" data-slidehub-current-page>0</span>
-      <span class="sh-page-widget__total-pages" data-slidehub-total-pages>0</span>
+  insertWidgetMarkup() {
+    const widgetMarkup = `<div class="sh-page-widget" aria-hidden="hidden">
+      Page
+      <span class="sh-page-widget__current-page" data-slidehub-current-page>0</span><span data-slidehub-total-pages>0</span>
     </div>`;
 
-    const hookNode = document.querySelector('[data-slidehub-page-widget]');
-    hookNode.insertAdjacentHTML('beforeend', widgetMarkup);
+    this.widgetHookNode.insertAdjacentHTML('beforeend', widgetMarkup);
 
-    this.totalPagesNode = hookNode.querySelector('[data-slidehub-total-pages]');
-    this.currentPageNode = hookNode.querySelector('[data-slidehub-current-page]');
+    const widgetNode = this.widgetHookNode.firstElementChild;
+    this.currentPageNode = widgetNode.firstElementChild;
+    this.totalPagesNode = widgetNode.lastElementChild;
 
     this.totalPagesNode.textContent = this.slidehub.selectedDocument.itemCount();
     this.currentPageNode.textContent = this.slidehub.selectedDocument.selectedItemNode.dataset.page;
   }
 
-  removeMarkup() {
-    const hookNode = document.querySelector('[data-slidehub-page-widget]');
-    hookNode.innerHTML = '';
+  removeWidgetMarkup() {
+    while (this.widgetHookNode.lastChild) {
+      this.widgetHookNode.removeChild(this.widgetHookNode.lastChild);
+    }
+
+    this.totalPagesNode = null;
+    this.currentPageNode = null;
   }
 
   exposeItemCount(event) {
