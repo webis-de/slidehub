18eaa338748ebd566dec20f9bba185d831c9f219
kleinfreund <phrudloff@gmail.com>
2018-07-21 10:06:15 +0200

    Update dependencies and webpack config (yet another breaking change)

--- webpack.common.js
+++ webpack.common.js
@@ -11,13 +11,38 @@ module.exports = {
         test: /\.css$/,
         exclude: /node_modules/,
         use: ExtractTextPlugin.extract({
-          loader: 'css-loader',
-          options: { sourceMap: true, minimize: true }
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+                importLoaders: 1
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                ident: 'postcss',
+                plugins: [
+                  require('cssnano')({
+                    preset: [
+                      'default',
+                      { mergeLonghand: false }
+                    ]
+                  })
+                ]
+              }
+            }
+          ]
         })
       }
     ]
   },
-  plugins: [new ExtractTextPlugin('[name].bundle.css')],
+  plugins: [
+    new ExtractTextPlugin('[name].bundle.css')
+  ],
   output: {
     path: path.resolve(__dirname, '.'),
     filename: '[name].bundle.js'
