c884ef6adf6ee290dfbb805b8898e80011029d9a
kleinfreund <phrudloff@gmail.com>
2018-08-29 21:07:39 +0200

    New build

--- slidehub.5c3b145c.js
+++ slidehub.5c3b145c.js
@@ -253,7 +253,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '37381' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '42089' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
